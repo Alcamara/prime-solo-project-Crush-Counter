ca5ea9492fc48e5e928ba783404fab51012123ff
Al Camara <Alhousseinec@gmail.com>
2022-07-06 04:51:04 -0500

    added logic to query data from api

--- database.sql
+++ database.sql
@@ -3,8 +3,41 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+CREATE TABLE "public.user" (
+	"id" integer NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"first_name" varchar(80) NOT NULL,
+	"last_name" varchar(80) NOT NULL,
+	"gamertag" varchar(80) NOT NULL,
+	"country" varchar(80) NOT NULL,
+	"state" varchar(80) NOT NULL,
+	"city" varchar(80) NOT NULL,
+	"zip_code" integer(5) NOT NULL,
+	"password" varchar(255) NOT NULL
+) 
+
+
+CREATE TABLE "public.matchNotes" (
+	"id" BINARY NOT NULL,
+	"win" BOOLEAN(80) NOT NULL,
+	"num_games" integer(80) NOT NULL,
+	"skillDemonstrated" varchar(80) NOT NULL,
+	"skillToImprove" varchar(80) NOT NULL,
+	"note" varchar(255) NOT NULL
+)
+
+
+
+CREATE TABLE "public.UserTournamentSurvey" (
+	"Id" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"surveyId" integer NOT NULL
+) 
+
+
+
+
+
+
+
+
---
+++ server/routes/tournaments.router.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+require('dotenv').config()
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+  const endpoint = "https://api.start.gg/gql/alpha";
+
+    const headers = {
+        "content-type": "application/json",
+        "Authorization": "Bearer "
+    };
+
+    const graphqlQuery = {
+        "query": `
+        query {
+          tournaments(query:{
+            perPage: 50
+            page:1
+            filter:{
+              hasBannerImages:true
+              location:{
+                distanceFrom:"44.92380657348581,-93.21992492794612"
+                distance:"1000mi"
+              }
+              past:false
+              upcoming: true
+              videogameIds: [7,10055]
+            }
+          }){
+            nodes{
+              id
+              name
+              isRegistrationOpen
+              isOnline
+              startAt
+              endAt
+              venueAddress
+              numAttendees
+              images {
+                id
+                url
+              }
+              
+              
+            }
+          }
+        }
+        `,
+    };
+
+    axios({
+        url:endpoint,
+        method:"POST",
+        headers:headers,
+        data: graphqlQuery
+    }).then((resp)=>{
+        console.log(resp.data.data.tournaments);
+        res.send(resp.data.data.tournaments)
+    }).catch((err)=>{
+        console.error(`${err}`);
+    })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
