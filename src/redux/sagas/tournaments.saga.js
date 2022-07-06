348c1d6d4239256317c9053159027347459ae692
Al Camara <Alhousseinec@gmail.com>
2022-07-06 07:29:17 -0500

    added comments

--- src/redux/sagas/tournaments.saga.js
+++ src/redux/sagas/tournaments.saga.js
@@ -1,10 +1,17 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+
+/*
+    function does an axios get request retrieve tournament data
+    from api and return it
+*/
 function* tournaments() {
     try {
-        const res = yield axios.get('/tournaments')
-        console.log(res.data);
+        
+        const res = yield axios.get('api/tournaments')
+        //console.log(res.data);
+        yield put({})
     } catch (err) {
         console.log(`axios get failed ${err}`);
     }
