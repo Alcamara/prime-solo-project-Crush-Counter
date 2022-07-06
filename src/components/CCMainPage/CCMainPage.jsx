80ba2aff513e912fc9bd04e3b664b0a7240d621f
Al Camara <Alhousseinec@gmail.com>
2022-07-06 11:06:26 -0500

    created tournament card

--- src/components/CCMainPage/CCMainPage.jsx
+++ src/components/CCMainPage/CCMainPage.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch,useSelector } from "react-redux"
 import CCHeader from "../CCHeader/CCHeader"
 import CCNav from "../CCNav/CCNav"
 
 export default function () {
     const dispatch = useDispatch()
+    const tournaments = useSelector(store => store.tournaments)
 
+   if(tournaments){
+    console.log(tournaments);
+    console.log(tournaments[0]);
+   }
+    
     useEffect(()=>{
         dispatch({
             type: 'FETCH_TOURNAMENTS_DATA'
@@ -16,6 +22,13 @@ export default function () {
         <div>
             <CCHeader/>
             <main>
+                <div>
+
+                    { tournaments && <ul>{tournaments.sort((a,b) =>( b.numAttendees - a.numAttendees )).map(tournament =>(
+                        <li key={tournament.id}>{tournament.numAttendees}</li>
+                    ))}</ul>}
+
+                </div>
                 
             </main>
         </div>
