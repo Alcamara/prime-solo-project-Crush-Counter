5dfac99565bb29aea9784dabcee08b424ccb6453
Al Camara <Alhousseinec@gmail.com>
2022-07-08 10:11:01 -0500

    created form

--- src/components/CCMatchNote/CCMatchNote.jsx
+++ src/components/CCMatchNote/CCMatchNote.jsx
@@ -3,14 +3,22 @@ import { useParams } from "react-router-dom"
 import {useSelector, useDispatch} from "react-redux"
 
 //MUI
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
-
+import { Button } from "@material-ui/core";
+import { Stack } from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 
 export default function CCMatchNote() {
     const {id} = useParams();
     const dispatch = useDispatch();
-    const [matchNote,setMatchNote] = useState({});
+    const [matchNote,setMatchNote] = useState({win:false,skillDemonstrated:"",skillToImprove:"",note:""});
     const tournamentTitle = useSelector(store => store.tournament);
     
     useEffect(()=>{
@@ -27,15 +35,73 @@ export default function CCMatchNote() {
             <h2>{tournamentTitle.name} Notes</h2>
             
             <div>
+            <h4>Did You Win The Match?</h4>
+               <Stack direction='row' spacing={2}>
+                    <Button variant="contained">
+                        YES
+                    </Button>
+                    <Button variant="outlined">
+                        NO
+                    </Button>
+               </Stack>
+            </div>
+            <div>
+                <h4>What Did You Do Well</h4>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Did well</InputLabel>
+                    <Select
+                    label="did-well"
+                    >
+                    <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
+                    <MenuItem value={'defense'}>Defense</MenuItem>
+                    <MenuItem value={'offense'}>Offense</MenuItem>
+                    <MenuItem value={'combo execution'}>Combo Execution</MenuItem>
+                    <MenuItem value={'spacing'}>Spacing</MenuItem>
+                    <MenuItem value={'footsies'}>Footsies</MenuItem>
+                    <MenuItem value={'punishes'}>Punishes</MenuItem>
+                    <MenuItem value={'Match up knowledge'}>Match Up Knowledge</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                <h4>What Did Not Go Well</h4>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Did well</InputLabel>
+                    <Select
+                    label="did-not"
+                    >
+                    <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
+                    <MenuItem value={'defense'}>Defense</MenuItem>
+                    <MenuItem value={'offense'}>Offense</MenuItem>
+                    <MenuItem value={'combo execution'}>Combo Execution</MenuItem>
+                    <MenuItem value={'spacing'}>Spacing</MenuItem>
+                    <MenuItem value={'footsies'}>Footsies</MenuItem>
+                    <MenuItem value={'punishes'}>Punishes</MenuItem>
+                    <MenuItem value={'Match up knowledge'}>Match Up Knowledge</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
             <TextField
                 id="filled-multiline-static"
                 label="Additional Notes"
                 multiline
+                fullWidth
                 rows={4}
-                placeholder="Added additional notes"
                 variant="filled"
             />
             </div>
+            <div>
+            <Stack direction='row' spacing={2}>
+                <Button variant="contained">
+                    SUBMIT
+                </Button>
+                <Button color="error" variant="outlined">
+                    CANCEL
+                </Button>
+            </Stack>
+            </div>
+
         </div>
     )
 }
