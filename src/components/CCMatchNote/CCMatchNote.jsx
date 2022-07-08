3966aa9a6c45ac7a9689fffbb692b682eea01f48
Al Camara <Alhousseinec@gmail.com>
2022-07-08 07:55:14 -0500

    Create CCMatchNote Componente

---
+++ src/components/CCMatchNote/CCMatchNote.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+
+export default function CCMatchNote() {
+    return (
+        <div>
+            <h2>hello</h2>
+        </div>
+    )
+}
