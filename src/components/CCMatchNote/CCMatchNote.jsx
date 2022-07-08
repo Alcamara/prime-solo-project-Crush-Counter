f063cfe564c0e00194c0ce8fb31a6336d24c9e6d
Al Camara <Alhousseinec@gmail.com>
2022-07-08 11:26:02 -0500

    styled match note

--- src/components/CCMatchNote/CCMatchNote.jsx
+++ src/components/CCMatchNote/CCMatchNote.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import {useSelector, useDispatch} from "react-redux"
 
+//css
+import './CCMatchNote.css'
+
 //MUI
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -31,73 +34,90 @@ export default function CCMatchNote() {
 
 
     return (
-        <div>
+        <div className='form'>
             <h2>{tournamentTitle.name} Notes</h2>
             
-            <div>
+            <div className='question'>
             <h4>Did You Win The Match?</h4>
-               <Stack direction='row' spacing={2}>
-                    <Button variant="contained">
-                        YES
-                    </Button>
-                    <Button variant="outlined">
-                        NO
-                    </Button>
-               </Stack>
-            </div>
-            <div>
-                <h4>What Did You Do Well</h4>
-                <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Did well</InputLabel>
-                    <Select
-                    label="did-well"
+                <div className="button1">
+                    <Stack 
+                        alignItems="center" 
+                        justifyContent="center" 
+                        direction='row' 
+                        spacing={2}
                     >
-                    <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
-                    <MenuItem value={'defense'}>Defense</MenuItem>
-                    <MenuItem value={'offense'}>Offense</MenuItem>
-                    <MenuItem value={'combo execution'}>Combo Execution</MenuItem>
-                    <MenuItem value={'spacing'}>Spacing</MenuItem>
-                    <MenuItem value={'footsies'}>Footsies</MenuItem>
-                    <MenuItem value={'punishes'}>Punishes</MenuItem>
-                    <MenuItem value={'Match up knowledge'}>Match Up Knowledge</MenuItem>
-                    </Select>
-                </FormControl>
+                        <Button variant="contained">
+                            YES
+                        </Button>
+                        <Button variant="outlined">
+                            NO
+                        </Button>
+                </Stack>
+                </div>
             </div>
-            <div>
-                <h4>What Did Not Go Well</h4>
-                <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Did well</InputLabel>
-                    <Select
-                    label="did-not"
-                    >
-                    <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
-                    <MenuItem value={'defense'}>Defense</MenuItem>
-                    <MenuItem value={'offense'}>Offense</MenuItem>
-                    <MenuItem value={'combo execution'}>Combo Execution</MenuItem>
-                    <MenuItem value={'spacing'}>Spacing</MenuItem>
-                    <MenuItem value={'footsies'}>Footsies</MenuItem>
-                    <MenuItem value={'punishes'}>Punishes</MenuItem>
-                    <MenuItem value={'Match up knowledge'}>Match Up Knowledge</MenuItem>
-                    </Select>
-                </FormControl>
+            <div className='question'>
+                <h4>What Did You Do Well?</h4>
+                <div className="dropdown">
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Did well</InputLabel>
+                        <Select
+                            label="did-not"
+                        >
+                        <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
+                        <MenuItem value={'defense'}>Defense</MenuItem>
+                        <MenuItem value={'offense'}>Offense</MenuItem>
+                        <MenuItem value={'combo'}>Combo Execution</MenuItem>
+                        <MenuItem value={'spacing'}>Spacing</MenuItem>
+                        <MenuItem value={'footsies'}>Footsies</MenuItem>
+                        <MenuItem value={'punishes'}>Punishes</MenuItem>
+                        <MenuItem value={'knowledge'}>Match Up Knowledge</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
             </div>
-            <div>
+            <div className='question'>
+                <h4>What Did Not Go Well?</h4>
+                <div className="dropdown">
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Didn't Go Well</InputLabel>
+                        <Select
+                            label="did-not"
+                        >
+                        <MenuItem value={'anti-air'}>Anti-Air</MenuItem>
+                        <MenuItem value={'defense'}>Defense</MenuItem>
+                        <MenuItem value={'offense'}>Offense</MenuItem>
+                        <MenuItem value={'combo execution'}>Combo Execution</MenuItem>
+                        <MenuItem value={'spacing'}>Spacing</MenuItem>
+                        <MenuItem value={'footsies'}>Footsies</MenuItem>
+                        <MenuItem value={'punishes'}>Punishes</MenuItem>
+                        <MenuItem value={'Match up knowledge'}>Match Up Knowledge</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+            </div>
+            <div className='question'>
             <TextField
+                className="textField"
                 id="filled-multiline-static"
                 label="Additional Notes"
                 multiline
-                fullWidth
+                
                 rows={4}
                 variant="filled"
             />
             </div>
-            <div>
-            <Stack direction='row' spacing={2}>
-                <Button variant="contained">
-                    SUBMIT
-                </Button>
+            <div className="button1">
+            <Stack 
+                alignItems="center" 
+                justifyContent="center" 
+                direction='row' 
+                spacing={2}
+            >
                 <Button color="error" variant="outlined">
                     CANCEL
+                </Button>
+                <Button variant="contained">
+                    SUBMIT
                 </Button>
             </Stack>
             </div>
