ca8302b082f8d88fd5e648e84cd722898de9453c
Al Camara <Alhousseinec@gmail.com>
2022-07-08 08:07:30 -0500

    added useEffect

--- src/components/CCMatchNote/CCMatchNote.jsx
+++ src/components/CCMatchNote/CCMatchNote.jsx
@@ -1,8 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import {useSelector, useDispatch} from "react-redux"
+
 
 
 export default function CCMatchNote() {
+    const dispatch = useDispatch();
+    const [matchNote,setMatchNote] = useState({})
+    const {id} = useParams()
+    useEffect(()=>{
+        console.log(id);
+        
+    },[id])
+
+
     return (
         <div>
             <h2>hello</h2>
