0ef18d1fe5397a12e314a690fdd06e547534465d
Al Camara <Alhousseinec@gmail.com>
2022-07-08 08:28:39 -0500

    added multi textfield

--- src/components/CCMatchNote/CCMatchNote.jsx
+++ src/components/CCMatchNote/CCMatchNote.jsx
@@ -2,21 +2,40 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import {useSelector, useDispatch} from "react-redux"
 
+//MUI
+import TextField from '@mui/material/TextField';
+
 
 
 export default function CCMatchNote() {
+    const {id} = useParams();
     const dispatch = useDispatch();
-    const [matchNote,setMatchNote] = useState({})
-    const {id} = useParams()
+    const [matchNote,setMatchNote] = useState({});
+    const tournamentTitle = useSelector(store => store.tournament);
+    
     useEffect(()=>{
         console.log(id);
-        
+        dispatch({
+            type:"FETCH_TOURNAMENT_DETAILS",
+            payload: id
+        })
     },[id])
 
 
     return (
         <div>
-            <h2>hello</h2>
+            <h2>{tournamentTitle.name} Notes</h2>
+            
+            <div>
+            <TextField
+                id="filled-multiline-static"
+                label="Additional Notes"
+                multiline
+                rows={4}
+                placeholder="Added additional notes"
+                variant="filled"
+            />
+            </div>
         </div>
     )
 }
